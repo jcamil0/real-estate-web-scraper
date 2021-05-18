d48c36f87bd33bbac2cc9a5b0d80f2917a6a5072
jcamil0 <jcamilo.llanos@hotmail.com>
2021-05-17 22:56:24 -0500

    script logic terminated

--- Scraping.py
+++ Scraping.py
@@ -2,22 +2,17 @@
 from bs4 import BeautifulSoup
 import requests
 import json
-import pandas as pd
 from selenium import webdriver
-from helper_functions import clean_tweet
-import time
-
-zone = {'norte': [], 'oeste': [], 'centro': [], 'sur': [], "oriente": []}
-urlid = ["8200101", "8200102", "8200103", "8200104", "8200105"]
-data = []
-urlLists = []
+import sys
 
 
-def getAllUrl():
-    for j in range(1):
+def getAllUrl(zone, urlid, pagination):
+    for j in range(pagination):
+        id = 0
         for i in zone.keys():
-            baseurl = 'https://www.fincaraiz.com.co/casas/venta/{}/cali/?ad=30|{}||||1||9|||82|8200006|8200101|||||||||||||||1|||1||griddate%20desc||||-1||||'.format(
-                i, j)
+            baseurl = 'https://www.fincaraiz.com.co/casas/venta/{}/cali/?ad=30|{}||||1||9|||82|8200006|{}|||||||||||||||1|||1||griddate%20desc||||-1||||'.format(
+                i, j, urlid[id])
+            id += 1
             page = requests.get(baseurl)
             soup = BeautifulSoup(page.content, 'html.parser')
             urls = soup.find_all('div', class_='span-title')
@@ -27,96 +22,94 @@ def getAllUrl():
                         zone[i].append(
                             'https://www.fincaraiz.com.co{}'.format(x['href']))
 
-            with open('./data/{}.json'.format(i), 'w', encoding='utf-8') as f:
-                json.dump(zone[i], f, ensure_ascii=False, indent=4)
-            print("se ha creado el archivo {}.json".format(i))
+        with open('./url/urls.json', 'w', encoding='utf-8') as f:
+            json.dump(zone, f, ensure_ascii=False, indent=4)
+        print("file urls.json created")
+        print("page {} ready !!".format(j+1))
 
 
-def scrapy_data():
-    url = 0
-    for items in zone.values():
-        print(items)
-        r = requests.get(items)
-        soup = BeautifulSoup(r.content, 'html.parser')
-        title = soup.find('div', class_='box').span
-        prices = soup.find('div', {'class': 'price'})
-        mts = soup.find('span', {'class': 'advertSurface'}).text.strip()
-        rooms = str(soup.find('span', {'class': 'advertRooms'}).text).replace(
-            "Habitaciones:", "")
-        baths = str(soup.find('span', {'class': 'advertBaths'}).text).replace(
-            "Baños:", "")
-        parking = str(soup.find('span', {'class': 'advertGarages'}).text).replace(
-            "Parqueaderos:", "")
+def scrapy_data(data):
+    for items in zone.keys():
+        home = 1
+        for x in zone[items]:
+            r = requests.get(x)
+            soup = BeautifulSoup(r.content, 'html.parser')
+            title = soup.find('div', class_='box').span
+            prices = soup.find('div', {'class': 'price'})
+            mts = soup.find('span', {'class': 'advertSurface'}).text.strip()
+            rooms = str(soup.find('span', {'class': 'advertRooms'}).text).replace(
+                "Habitaciones:", "")
+            baths = str(soup.find('span', {'class': 'advertBaths'}).text).replace(
+                "Baños:", "")
+            parking = str(soup.find('span', {'class': 'advertGarages'}).text).replace(
+                "Parqueaderos:", "")
 
-        interior = soup.find("ul", {"id": "tblInitialInteriores"})
-        interiors = []
-        if interior:
-            newsoup = BeautifulSoup(str(interior), 'html.parser')
-            lis = newsoup.find_all('li')
-            for li in lis:
-                interiors.append(li.text)
+            interior = soup.find("ul", {"id": "tblInitialInteriores"})
+            interiors = []
+            if interior:
+                newsoup = BeautifulSoup(str(interior), 'html.parser')
+                lis = newsoup.find_all('li')
+                for li in lis:
+                    interiors.append(li.text)
 
-        ext = soup.find("ul", {"id": "tblInitialExteriores"})
-        _ext = []
-        if ext:
-            newsoup = BeautifulSoup(str(ext), 'html.parser')
-            lis = newsoup.find_all('li')
-            for li in lis:
-                _ext.append(li.text)
+            ext = soup.find("ul", {"id": "tblInitialExteriores"})
+            _ext = []
+            if ext:
+                newsoup = BeautifulSoup(str(ext), 'html.parser')
+                lis = newsoup.find_all('li')
+                for li in lis:
+                    _ext.append(li.text)
 
-        sector = soup.find("ul", {"id": "tblInitialdelSector"})
-        _sector = []
-        if sector:
-            newsoup = BeautifulSoup(str(sector), 'html.parser')
-            lis = newsoup.find_all('li')
-            for li in lis:
-                _sector.append(li.text)
+            sector = soup.find("ul", {"id": "tblInitialdelSector"})
+            _sector = []
+            if sector:
+                newsoup = BeautifulSoup(str(sector), 'html.parser')
+                lis = newsoup.find_all('li')
+                for li in lis:
+                    _sector.append(li.text)
 
-        data.append({"title": title.text,
-                     "price": prices.text.strip,
-                     "mts": mts,
-                     "rooms": rooms.strip(),
-                     "baths": baths.strip(),
-                     "parking": parking.strip(),
-                     #  "url": urlLists[url],
-                     "interior": interiors,
-                     "Exterior": _ext,
-                     "around": _sector
-                     })
-        # # url += 1
-        # # if url == 10:
-        # #     break
-    # WriteData()
-
-
-def features():
-    print()
+            data.update({
+                "home {}".format(home): {
+                    "title": title.text,
+                    "price": prices.text.strip(),
+                    "mts": mts,
+                    "rooms": rooms.strip(),
+                    "baths": baths.strip(),
+                    "parking": parking.strip(),
+                    "interior": interiors,
+                    "Exterior": _ext,
+                    "around": _sector,
+                    "url": x,
+                }})
+            home += 1
+        data["total"] = len(data)
+        WriteData(data, items)
 
 
-def WriteData():
-    with open('/data/hola.json', 'w', encoding='utf-8') as f:
+def WriteData(data, items):
+    with open('./data/{}.json'.format(items), 'w', encoding='utf-16') as f:
         json.dump(data, f, ensure_ascii=False, indent=4)
-        print("se ha creado el archivo data.json")
-
-
-def NorteData():
-    norteDf = pd.read_json('./data/norte.json', orient='records')
-    norteDf['clean_tweet'] = norteDf['price'].apply(lambda x: clean_tweet(x))
-    norteDf['item'] = 'Bitcoin'
-    norteDf.drop_duplicates(subset=['title'], keep='first', inplace=True)
-    return norteDf
+        print("file {}.json ready!!".format(items))
 
 
 if __name__ == "__main__":
-    # print("recolectando datos....")
-    getAllUrl()
-    # scrapy_data()
+    print("recolectando datos....")
+    zone = {'norte': [], 'oeste': [], 'centro': [], 'sur': [], "oriente": []}
+    urlid = ["8200101", "8200102", "8200103", "8200104", "8200105"]
+    data = {}
+    pagination = 1
+
+    if len(sys.argv) == 1:
+        getAllUrl(zone, urlid, pagination)
+    else:
+        pagination = int(sys.argv[1])
+        getAllUrl(zone, urlid, pagination)
+
+    scrapy_data(data)
+
+    print("bye....")
     drive = webdriver.Chrome(
         "/home/camilo/Documentos/bigdata/proyecto/page/chromedriver")
     drive.get("http://0.0.0.0:8000/page/")
 
-
-# drive = webdriver.Chrome(
-#     "/home/camilo/Documentos/bigdata/proyecto/page/chromedriver")
-# drive.get("http://0.0.0.0:8000/page/")
-# python -m SimpleHTTPServer 8000
+    # python -m SimpleHTTPServer 8000
